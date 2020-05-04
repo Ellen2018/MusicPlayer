4d75c511bb54bcdae67982889b615578007c246c
陈浪 <chenlang@sx985.com>
2020-05-04 20:56:37 +0800

    修复我喜欢不能添加多首歌曲的bug

--- app/src/main/java/com/ellen/musicplayer/KeyValueTag.java
+++ app/src/main/java/com/ellen/musicplayer/KeyValueTag.java
@@ -4,6 +4,7 @@ public interface KeyValueTag {
 
     String PI_FU_NAME = "pi_fu";
     String PI_FU_KEY = "pi_fu_key";
+    
     String MUSIC_PLAY_NAME =  "music_play_name";
     String PLAY_MODE_KEY = "play_mode";
 
