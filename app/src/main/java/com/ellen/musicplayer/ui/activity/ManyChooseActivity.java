6fe5aa37f2f47fcefe444a7058221e8c1429ffbf
陈浪 <chenlang@sx985.com>
2020-05-11 17:14:13 +0800

    完成我喜欢 & 歌单 多选移除

--- app/src/main/java/com/ellen/musicplayer/ui/activity/ManyChooseActivity.java
+++ app/src/main/java/com/ellen/musicplayer/ui/activity/ManyChooseActivity.java
@@ -258,6 +258,7 @@ public class ManyChooseActivity extends BaseMediaPlayerActivity implements View.
 
                             }
                         });
+                        
                         commonOkCancelDialog.show(getSupportFragmentManager(),"");
                     }else {
                         //从歌单里进行删除
